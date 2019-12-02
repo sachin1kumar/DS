35574845914c2b4a71d80ecab005a802b72049c6
Sachin <sachinkmr375@gmail.com>
2019-12-02 22:50:24 +0530

    creating graph...

---
+++ src/graph/BSF.kt
@@ -0,0 +1,22 @@
+package graph
+
+fun main() {
+
+    val visited = BooleanArray(7)
+    for (i in 0 until visited.size){
+        visited[i] = false
+    }
+
+    //created graph..
+    createGraph()
+
+
+}
+
+fun createGraph() {
+
+}
+
+fun BSF(){
+
+}
