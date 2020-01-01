439e92e7b2b8ca7c8b45dadb7f949232f46e696d
Sachin <sachinkmr375@gmail.com>
2020-01-01 22:45:23 +0530

    insertion linkedlist problem

---
+++ src/linkedlist/Insertion.kt
@@ -0,0 +1,60 @@
+package linkedlist
+
+fun main() {
+
+    var firstNode = Node(1)
+    var secondNode = Node(2)
+    var thirdNode = Node(3)
+    var fourthNode = Node(4)
+
+
+    firstNode.next = secondNode
+    secondNode.next = thirdNode
+    thirdNode.next = fourthNode
+
+    //print
+
+    printMtd(insertNodeAtPosition(firstNode,122,2))
+
+}
+
+fun printMtd(llist: Node?){
+    var head: Node? = llist
+    while (head!=null){
+        println(head.data)
+        head = head.next
+    }
+}
+
+fun insertNodeAtPosition(llist: Node?, data: Int, position: Int): Node? {
+
+    if(position == -1){
+        return llist
+    }
+
+    if(llist == null){
+        return null
+    }
+
+    var pos = 0
+    var pointer: Node? = llist
+
+    while(pos < position){
+        pointer = pointer?.next
+        pos++
+    }
+
+    val temp = pointer
+
+    val newList = Node(data)
+    pointer = newList
+    //println(pointer.data)
+    //println(temp?.data)
+
+    pointer?.next = temp
+    //println(pointer?.next?.data)
+
+
+    return llist
+
+}
