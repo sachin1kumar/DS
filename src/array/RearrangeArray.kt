44492766781267e2b66708b030e438150da66767
Sachin <sachinkmr375@gmail.com>
2020-09-26 23:24:48 +0530

    RearrangeArray

---
+++ src/array/RearrangeArray.kt
@@ -0,0 +1,13 @@
+package array
+
+fun main() {
+
+    val arr = intArrayOf(4, 0, 2, 1, 3)
+
+    //TODO
+    //modification of array is pending..
+
+    for (index in arr.indices) {
+        print("${arr[arr[index]]} ")
+    }
+}
