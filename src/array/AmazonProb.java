18c8c647a75c9e860f49d0e8ae36b84a63d28856
Sachin <sachinkmr375@gmail.com>
2020-10-12 19:23:04 +0530

    Amazon interview problem.

---
+++ src/array/AmazonProb.java
@@ -0,0 +1,38 @@
+package array;
+
+import java.util.ArrayList;
+
+public class AmazonProb {
+
+    public static void main(String[] args) {
+        String s = "|**|*|";
+        int strCount = 0;
+        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Character> stack = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '*') {
+                strCount++;
+            }
+            if (s.charAt(i) == '|') {
+                stack.add('|');
+            }
+            if (stack.size() == 2) {
+                res.add(strCount);
+                stack.remove(1);
+                strCount = 0;
+            }
+            if (stack.size() == 0) {
+                strCount = 0;
+            }
+        }
+
+        //print out
+        int sum = 0;
+        for (int j = 0; j < res.size(); j++) {
+            sum += res.get(j);
+        }
+
+        System.out.println(sum);
+    }
+}
