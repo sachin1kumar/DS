6c1f68d7fb060819a3456a0f1e5b86e33bbebd5a
Sachin <sachinkmr375@gmail.com>
2020-11-13 23:21:31 +0530

    Rearrange positive and negative numbers with constant extra space.

---
+++ src/array/SegregatePositiveNegative.kt
@@ -0,0 +1,43 @@
+package array
+
+fun main() {
+    val inputArray = intArrayOf(12, 11, -13, -5, 6, -7, 5, -3, -6)
+
+    //Can we solve using sorting algorithm but will take O(nlogn) time complexity..
+
+    //we can also use the approach, keep on checking for the negative number and when encounter it save that number and we
+    //have one variable for maintaing the index for shifting the number to a particular index and in this case it's 0 initially so shift it there
+    //and shift all the preceding elements in the array to the next one index. But time complexity is O(n2)
+
+    //Solving in O(n) using two pointer approach..
+    var i = 0
+    var j = inputArray.size - 1
+
+    while (true) {
+        //keep on increment i..
+        while (i < j && inputArray[i] < 0) {
+            i++
+        }
+
+        //Keep on decrement j..
+        while (i < j && inputArray[j] > 0) {
+            j--
+        }
+
+        if (i < j) {
+            val temp = inputArray[i]
+            inputArray[i] = inputArray[j]
+            inputArray[j] = temp
+            i++
+            j--
+        } else {
+            break
+        }
+    }
+
+    //print output..
+    for (element in inputArray) {
+        print("$element ")
+    }
+
+}
