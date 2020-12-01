672cb8bdd26b9538ba9c6be0ddaf9ecabb08a6aa
Sachin <sachinkmr375@gmail.com>
2020-12-01 23:16:55 +0530

    kth largest element in array..

--- src/array/KthElement.kt
+++ src/array/KthElement.kt
@@ -7,11 +7,22 @@ fun main() {
     val priorityQueue = PriorityQueue<Int>()
 
     //finding kth largest...
-    val k = 4
+    val k = 3
 
     //It is using min heap approach with time complexity O(1)..
     // 7 10 4 3, then when the size of heap exceeds four then 3 will be removed and then 4 will be removed and
-    // eventually it will return us 7..
+    // eventually it will return us 7 as it is top elemtn in the heap..
+    // sequence will be:
+    // 7
+    // 7 10
+    // 4 7 10
+    // 3 4 7 10
+    // 4 7 10
+    // 4 7 10 20
+    // 7 10 20
+    // 7 10 15 20
+    // 10 15 20
+    // 15 20 -> then has hot removed and printed outside the for loop..
     for (index in 0 until input.size) {
         priorityQueue.add(input[index])
         if (priorityQueue.size > k) {
