597c0746209dc977959e57c9b733dd18f53205f3
Sachin <sachinkmr375@gmail.com>
2021-11-05 12:55:20 +0530

    jump problem

--- src/array/JumpProblem.java
+++ src/array/JumpProblem.java
@@ -5,7 +5,6 @@ public class JumpProblem {
     public static void main(String[] args) {
         //int[] arr = {1, 1, 2, 5, 2, 1, 0, 0, 1, 3};
         int[] arr = {1, 2, 1, 0, 2};
-
         System.out.println(checkArray(arr));
     }
 
