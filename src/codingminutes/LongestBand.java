1337889791c32bbf2cfae3b84857c652e3c8c079
Sachin <sachinkmr375@gmail.com>
2021-12-15 19:06:22 +0530

    Longest band problem

---
+++ src/codingminutes/LongestBand.java
@@ -0,0 +1,85 @@
+package codingminutes;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestBand {
+    public static void main(String[] args) {
+        int[] input = {1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6};
+        System.out.println(getLongestBand(input));
+        System.out.println(getLongestBandImproved(input));
+    }
+
+    /*
+     * Time complexity- O(NlogN)
+     * Space complexity - O(1)
+     * */
+    private static int getLongestBand(int[] input) {
+        int length = input.length;
+
+        if (length == 0) {
+            return -1;
+        }
+
+        if (length == 1) {
+            return 1;
+        }
+
+        //Sort the array - O(NlogN)
+        Arrays.sort(input);
+
+        int longest = 0;
+        int count = 1;
+        //O(N)
+        for (int index = 1; index < length; index++) {
+            if (input[index] - input[index - 1] == 1) {
+                count++;
+                longest = Math.max(longest, count);
+            } else {
+                count = 1;
+            }
+        }
+        return longest;
+    }
+
+    /*
+     * Time complexity- O(N)
+     * Space complexity - O(N)
+     * */
+    private static int getLongestBandImproved(int[] input) {
+        int length = input.length;
+
+        if (length == 0) {
+            return -1;
+        }
+
+        if (length == 1) {
+            return 1;
+        }
+
+        //Store the elements in hashset in O(N).
+        final Set<Integer> set = new HashSet<>();
+        for (int index = 0; index < length; index++) {
+            set.add(input[index]);
+        }
+
+        int longest = 0;
+
+        //Iterate input array in O(N).
+        for (int index = 0; index < length; index++) {
+            if (set.contains(input[index] - 1)) {
+                continue;
+            } else {
+                int j = index;
+                int count = 1;
+                while (j < length - 1 && set.contains(input[j] + 1)) {
+                    count++;
+                    j++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+}
