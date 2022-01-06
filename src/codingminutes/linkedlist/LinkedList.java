844f16a9cbc6d2ec5c19140f9091762f090f8282
Sachin <sachinkmr375@gmail.com>
2022-01-06 19:04:47 +0530

    LinkedList implementation.

---
+++ src/codingminutes/linkedlist/LinkedList.java
@@ -0,0 +1,12 @@
+package codingminutes.linkedlist;
+
+public class LinkedList {
+
+    public int data;
+    public LinkedList next;
+
+    public LinkedList(int data) {
+        this.data = data;
+        next = null;
+    }
+}
