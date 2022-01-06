43526ff29d0b969e4ae7963e702fd6a1e6506b54
Sachin <sachinkmr375@gmail.com>
2022-01-06 22:08:53 +0530

    LinkedList insertion at the desired position.

--- src/codingminutes/linkedlist/BasicLinkedList.java
+++ src/codingminutes/linkedlist/BasicLinkedList.java
@@ -9,6 +9,9 @@ public class BasicLinkedList {
         LinkedList updatedHead = insertAtHead(1);
         LinkedList updatedHead_ = insertAtHead(2);
         printList(updatedHead_);
+        System.out.println("Insert the element");
+        LinkedList updatedHead2 = insertAtPos(updatedHead_, 3, 1);
+        printList(updatedHead2);
     }
 
     private static LinkedList insertAtHead(int data) {
@@ -17,7 +20,7 @@ public class BasicLinkedList {
             return head;
         }
 
-        LinkedList newList = new LinkedList(data);
+        final LinkedList newList = new LinkedList(data);
         newList.next = head;
         head = newList;
         return head;
@@ -35,5 +38,43 @@ public class BasicLinkedList {
         System.out.println();
     }
 
+    private static LinkedList insertAtPos(LinkedList head, int data, int pos) {
+        // 0 1 2
+        if (head != null && pos < 0) {
+            System.out.println("Pos is invalid");
+            return head;
+        }
 
+        if (head == null && pos < 0) {
+            System.out.println("Current list is empty and pos passed is invalid.");
+            return null;
+        }
+
+        if (head == null && pos > 0) {
+            System.out.println("Current list is empty and pos passed is greater than 0.");
+            return null;
+        }
+
+        int currentPos = 0;
+        LinkedList temp = head;
+        LinkedList prev = null;
+
+        while (temp != null && currentPos != pos) {
+            currentPos += 1;
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (prev != null) {
+            LinkedList newList = new LinkedList(data);
+            LinkedList temp_ = prev.next;
+            prev.next = newList;
+            newList.next = temp_;
+        } else {
+            LinkedList newList = new LinkedList(data);
+            newList.next = head;
+            head = newList;
+        }
+        return head;
+    }
 }
