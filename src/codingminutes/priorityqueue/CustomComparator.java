faf3f435690afbbbab721a9d6edcca8fd163df9f
Sachin <sachinkmr375@gmail.com>
2022-01-15 14:49:26 +0530

    CustomComparator problem.

---
+++ src/codingminutes/priorityqueue/CustomComparator.java
@@ -0,0 +1,10 @@
+package codingminutes.priorityqueue;
+
+import java.util.Comparator;
+
+public class CustomComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+    }
+}
